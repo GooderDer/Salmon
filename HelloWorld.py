fef2ac0c1808f73c83547549a1470cd8d91ab51e
congyuan_yu <1404034862@qq.com>
2026-03-01 14:25:17 +0800

    例题1

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,10 @@
 print("Hello World 老谈")
 
-str_first = "tan jiu jiu"
-a = str_first.split(" ")
-for x in a:
-    print(x)
+# str_first = "tan jiu jiu"
+# a = str_first.split(" ")
+# for x in a:
+#     print(x)
+
+# 例题1：将test_str去掉所有数字，然后分行打印
+
+test_str = "你1是1我1的1小1宝1贝，如2果2不2是2的2话，请3你3变3成3我3的3小3宝3贝"
